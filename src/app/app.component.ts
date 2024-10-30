92b7b3c5efe793e12609b8278d359143dc056abe
swagitbit <vasaviswathi78@gmail.com>
2024-10-30 21:52:32 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { ServicesComponent } from './components/services/services.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { CallComponent } from './components/call/call.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CallComponent,
+    FeaturesComponent,
+    FooterComponent,
+    HeaderComponent,
+    HeroComponent,
+    ServicesComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'insuranceapp';
